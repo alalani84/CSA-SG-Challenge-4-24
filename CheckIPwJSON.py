028841e2535f0a7660041568759e13c4937f8041
alalani84 <alalani84@gmail.com>
2020-04-27 23:11:01 -0400

    refactor code

--- CheckIPwJSON.py
+++ CheckIPwJSON.py
@@ -5,15 +5,15 @@ deviceJSON = '{"Version": "15.6", "locationN": "500 Northridge", "role": "Access
 
 data = json.loads(deviceJSON)
 
-gig0 = data["interfaces"]["interface"][0]["GigabitEthernet0"]["ipv4"]
-gig1 = data["interfaces"]["interface"][1]["GigabitEthernet1"]["ipv4"]
-ip = {"GigabitEthernet0":gig0, "GigabitEthernet1":gig1}
+int_ip = data["interfaces"]["interface"]
 
-for interface, address in ip.items():
-    if ipaddress.IPv4Address(address).is_private:
-        print("{} has an ip address of {}".format(interface, address) + " is a private ip address")
-    else:
-        print("{} has an ip address of {}".format(interface, address) + " is not a private ip address")
+for int in int_ip:
+    for interface, address in int.items():
+        add = address["ipv4"]
+        if ipaddress.IPv4Address(add).is_private:
+            print("{} has an ip address of {}".format(interface, add) + " is a private ip address")
+        else:
+            print("{} has an ip address of {}".format(interface, add) + " is not a private ip address")
 
 
 
